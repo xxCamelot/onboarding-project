832612660da30d37dfef94ead37570e3ee67d7f5
Camillo De Gregorio <cdegre04@gmail.com>
2022-10-26 16:06:14 +0100

    initial commit

---
+++ OnboardingProject/OnboardingProject/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnboardingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
