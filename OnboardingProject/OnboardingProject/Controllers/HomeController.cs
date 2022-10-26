8f6775377d1c80bee652e83cdfbb4590d322f522
Camillo De Gregorio <cdegre04@gmail.com>
2022-10-26 19:09:57 +0100

    list of properties
    completed page where all properties are displayed as a list

--- OnboardingProject/OnboardingProject/Controllers/HomeController.cs
+++ OnboardingProject/OnboardingProject/Controllers/HomeController.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OnboardingProject;
 using OnboardingProject.Models;
 
 namespace OnboardingProject.Controllers
 {
+
     public class HomeController : Controller
     {
+        private readonly List<PropertyModel> propertyList = new List<PropertyModel>();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -20,11 +24,13 @@ namespace OnboardingProject.Controllers
 
         public IActionResult Index()
         {
+            
             return View();
         }
 
         public IActionResult AddProperty()
         {
+            
             return View();
         }
 
@@ -35,10 +41,19 @@ namespace OnboardingProject.Controllers
 
         public IActionResult ListProperties()
         {
+            propertyList.Add(new PropertyModel("Viale F. Scaduto 10B", 105.7, new DescriptionModel("The house where I live", "La casa dove vivo", "Dom gdzie ja mieszkam"), "Laundry, Fridge, Wi-Fi, Pets, Balcony, TV"));
+            propertyList.Add(new PropertyModel("Via dell'Arsenale 132", 492.2, new DescriptionModel("My old home", "Mia vecchia casa", "Mòj stary dom"), "Fridge, Wi-Fi"));
+
+            ViewBag.propertyList = propertyList;
             return View();
         }
 
         public IActionResult DeleteProperty()
+        {
+            return View();
+        }
+
+        public IActionResult PropertyDescription()
         {
             return View();
         }
@@ -50,4 +65,3 @@ namespace OnboardingProject.Controllers
         }
     }
 }
-
--- OnboardingProject/OnboardingProject/Models/DescriptionModel.cs
+++ OnboardingProject/OnboardingProject/Models/DescriptionModel.cs
@@ -7,6 +7,13 @@ namespace OnboardingProject
         public string italianDescription { get; set; }
         public string polishDescription { get; set; }
 
+        public DescriptionModel(string e, string i, string p)
+        {
+            englishDescription = e;
+            italianDescription = i;
+            polishDescription = p;
+        }
+
         public DescriptionModel() { }
 
     }
